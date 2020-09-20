49af4ff6cd735902219b02adc647afcacd025406
tranthai <tranthai@vt.edu>
2020-09-20 01:15:03 -0400

    Add in key for NYTimes API

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "jHr7kHeLrZVYIH1uCGe5dkVL7GbelAQf";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
